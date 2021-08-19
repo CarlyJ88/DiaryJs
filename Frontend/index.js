d24c1bf563ca7e746cb895c8ec98b102599a81f1
Carly <carly.jenkinson65@googlemail.com>
2021-08-19 21:05:18 +0100

    Can add title. Refactor code.

--- Frontend/index.js
+++ Frontend/index.js
@@ -4,25 +4,30 @@ import axios from 'axios';
 form6.addEventListener('submit', (event) => {
   event.preventDefault();
   console.log(event, 'event')
+  console.log(title, 'title')
+  title.innerText = titleInput.value;
   todaysThoughts.innerHTML = myTextArea.value;
 });
-
+console.log(title, 'title outside')
 // get diary entries
 axios.get('http://localhost:4000/list')
 .then(res => res.data)
 .then(listEntries)
 
-function belongsTogether(element, item){
+function belongsTogether(element, item, newdiv){
   let newElement = document.createElement(element);
   newElement.append(item);
-  body.append(newElement);
+  newdiv.append(newElement);
 }
 
 function listEntries(diaryEntries) {
   for (let i = 0; i < diaryEntries.length; i++){
-    belongsTogether('cite', diaryEntries[i].title);
-    belongsTogether('pre', diaryEntries[i].entry);
-    belongsTogether('pre', diaryEntries[i].date);
+    const newdiv = document.createElement('div');
+    newdiv.style = "text-align: center";
+    belongsTogether('cite', diaryEntries[i].title, newdiv);
+    belongsTogether('pre', diaryEntries[i].entry, newdiv);
+    belongsTogether('pre', diaryEntries[i].date, newdiv);
+    body.append(newdiv);
   }
 }
 
