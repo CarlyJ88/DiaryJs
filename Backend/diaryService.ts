e914ec9baba1bf2ce2f66d9e748348508e57e609
Carly <carly.jenkinson65@googlemail.com>
2021-09-10 00:53:05 +0100

    Update edit service to show date entry is edited.

--- Backend/diaryService.ts
+++ Backend/diaryService.ts
@@ -12,12 +12,12 @@ interface EntriesSaved extends Entries {
 
 export async function listEntries(): Promise<EntriesSaved[]> {
   const entries = await executeQuery('SELECT * FROM entries', []);
-  return entries.rows.map((row: any) => ({ id: row.id, title: row.title, entry: row.entry, date: row.date }))
+  return entries.rows.map((row: any) => ({ id: row.id, title: row.title, entry: row.entry, date: row.date,  }))
 }
 
 export async function addEntry(entries: Entries): Promise<EntriesSaved> {
   const entry = await executeQuery('INSERT INTO entries(title, entry) VALUES($1, $2) RETURNING *', [entries.title, entries.entry]);
-  return { id: entry.rows[0].id, title: entry.rows[0].title, entry: entry.rows[0].entry, date: entry.rows[0].date };
+  return { id: entry.rows[0].id, title: entry.rows[0].title, entry: entry.rows[0].entry, date: entry.rows[0].date};
 }
 
 export async function deleteEntry(id: number): Promise<void> {
@@ -25,6 +25,6 @@ export async function deleteEntry(id: number): Promise<void> {
 }
 
 export async function editEntry(entries: EntriesSaved): Promise<EntriesSaved> {
-  const entry = await executeQuery('UPDATE entries SET title = $1, entry = $2 WHERE id = $3 RETURNING *', [entries.title, entries.entry, entries.id]);
+  const entry = await executeQuery('UPDATE entries SET title = $1, entry = $2, edited = CURRENT_DATE WHERE id = $3 RETURNING *', [entries.title, entries.entry, entries.id]);
   return { id: entry.rows[0].id, title: entry.rows[0].title, entry: entry.rows[0].entry, date: entry.rows[0].date };
 }
