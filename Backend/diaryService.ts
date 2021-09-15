7d381521370c28da57248f076d0fab392d49435f
Carly <carly.jenkinson65@googlemail.com>
2021-09-15 22:31:48 +0200

    DIARY-25 | Fix edited date when adding and editing entry.

--- Backend/diaryService.ts
+++ Backend/diaryService.ts
@@ -3,11 +3,12 @@ import executeQuery from "./db"
 interface Entries {
   title: string;
   entry: string;
-  date: string;
 }
 
 interface EntriesSaved extends Entries {
   id: number;
+  date: string;
+  edited: string;
 }
 
 export async function listEntries(): Promise<EntriesSaved[]> {
@@ -17,7 +18,7 @@ export async function listEntries(): Promise<EntriesSaved[]> {
 
 export async function addEntry(entries: Entries): Promise<EntriesSaved> {
   const entry = await executeQuery('INSERT INTO entries(title, entry) VALUES($1, $2) RETURNING *', [entries.title, entries.entry]);
-  return { id: entry.rows[0].id, title: entry.rows[0].title, entry: entry.rows[0].entry, date: entry.rows[0].date};
+  return { id: entry.rows[0].id, title: entry.rows[0].title, entry: entry.rows[0].entry, date: entry.rows[0].date, edited: entry.rows[0].edited };
 }
 
 export async function deleteEntry(id: number): Promise<void> {
@@ -26,5 +27,5 @@ export async function deleteEntry(id: number): Promise<void> {
 
 export async function editEntry(entries: EntriesSaved): Promise<EntriesSaved> {
   const entry = await executeQuery('UPDATE entries SET title = $1, entry = $2, edited = CURRENT_DATE WHERE id = $3 RETURNING *', [entries.title, entries.entry, entries.id]);
-  return { id: entry.rows[0].id, title: entry.rows[0].title, entry: entry.rows[0].entry, date: entry.rows[0].date };
+  return { id: entry.rows[0].id, title: entry.rows[0].title, entry: entry.rows[0].entry, date: entry.rows[0].date, edited: entry.rows[0].edited };
 }
