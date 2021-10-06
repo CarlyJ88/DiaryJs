7a71cdd1de6c36d7aa0095748c0b6fb9fdea0811
Carly <carly.jenkinson65@googlemail.com>
2021-10-06 15:17:54 +0100

    DIARY-31 | Change endpoint to serve current ip address so that data can be viewed via mobile.

--- Frontend/service.js
+++ Frontend/service.js
@@ -1,19 +1,22 @@
 import axios from 'axios';
 
+const base = window.location.host.split(':')[0];
+
 export function getEntries() {
-  return axios.get('http://localhost:4000/list')
+  return axios.get(`http://${base}:4000/list`)
     .then(res => res.data)
 }
 
 export function addEntry(title, entry) {
-  return axios.post('http://localhost:4000/add-entry', {
+  return axios.post(`http://${base}:4000/add-entry`, {
     title: title,
     entry: entry
   })
 }
 
 export function deleteEntry(id) {
-  return axios.delete('http://localhost:4000/delete-entry', {
+  const base = window.location.host.split(':')[0];
+  return axios.delete(`http://${base}:4000/delete-entry`, {
     data: {
       id: id
     },
@@ -21,7 +24,7 @@ export function deleteEntry(id) {
 }
 
 export function editEntry(title, entry, id) {
-  return axios.put('http://localhost:4000/edit-entry', {
+  return axios.put(`http://${base}:4000/edit-entry`, {
       title: title,
       entry: entry,
       id: id
