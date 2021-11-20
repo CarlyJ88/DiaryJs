5d24e52bca9dacbf551cdaabc095eedd5d0fade7
Carly <carly.jenkinson65@googlemail.com>
2021-11-20 18:38:49 +0000

    Refactor header component.

--- Frontend/header.js
+++ Frontend/header.js
@@ -1,28 +1,38 @@
 import back from './arrowback_111142.png';
 import write from './new.png';
 
-export default function header() {
+const title = () => {
   let title = document.createElement('h2');
   title.id = 'header-title'
+  title.innerHTML = "Something";
+  return title;
+}
+
+const backButton = () => {
   let backButton = document.createElement('img');
-  let newItemButton = document.createElement('img');
   backButton.src = back;
   backButton.alt = "Back button";
   backButton.width = 40;
   backButton.height = 40;
   backButton.id = 'header-back-button'
+  return backButton;
+}
 
+const newItemButton = () => {
+  let newItemButton = document.createElement('img');
   newItemButton.src = write;
   newItemButton.alt = "New item";
   newItemButton.width = 40;
   newItemButton.height = 40;
   newItemButton.id = 'header-new-item-button'
+  return newItemButton;
+}
+
+export default function header() {
   const headers = document.createElement('div');
-  title.innerHTML = "Something";
   headers.className = 'headers';
-  // headers.appendChild(title);
-  headers.append(backButton);
-  headers.append(title);
-  headers.append(newItemButton);
+  headers.append(backButton());
+  headers.append(title());
+  headers.append(newItemButton());
   body.append(headers);
 }
