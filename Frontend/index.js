5b935d2ecf8925fd737f11be4f061970a289cdba
Carly <carly.jenkinson65@googlemail.com>
2022-01-11 18:02:03 +0000

    Outline for calendar.

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,69 +1,76 @@
-
-import listEntriesPage from './views/listEntriesPage';
-import header from './header';
-import chooseCategory from './views/chooseCategoryPage';
-import showEntryPage from './views/showEntryPage';
-import createEntryPage from './views/createEntryPage';
+import listEntriesPage from "./views/listEntriesPage";
+import header from "./header";
+import chooseCategory from "./views/chooseCategoryPage";
+import showEntryPage from "./views/showEntryPage";
+import createEntryPage from "./views/createEntryPage";
+import calendarPage from "./views/calendarPage";
 
 import "regenerator-runtime/runtime.js";
 
 header();
 
-const pathToRegex = path => new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
+const pathToRegex = (path) =>
+  new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
 
-const getParams = match => {
-    const values = match.result.slice(1);
-    const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1]);
+const getParams = (match) => {
+  const values = match.result.slice(1);
+  const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(
+    (result) => result[1]
+  );
 
-    return Object.fromEntries(keys.map((key, i) => {
-        return [key, values[i]];
-    }));
+  return Object.fromEntries(
+    keys.map((key, i) => {
+      return [key, values[i]];
+    })
+  );
 };
 
-const navigateTo = url => {
-    history.pushState(null, null, url);
-    router();
+const navigateTo = (url) => {
+  history.pushState(null, null, url);
+  router();
 };
 
 const router = async () => {
   const routes = [
     //   { path: "/", view: Dashboard },
-      { path: "/choose", view: chooseCategory },
-      { path: "/list", view: listEntriesPage },
-      { path: "/show", view: showEntryPage },
-      { path: "/new", view: createEntryPage },
+    { path: "/choose", view: chooseCategory },
+    { path: "/list", view: listEntriesPage },
+    { path: "/show", view: showEntryPage },
+    { path: "/new", view: createEntryPage },
+    { path: "/calendar", view: calendarPage },
   ];
 
   // Test each route for potential match
-  const potentialMatches = routes.map(route => {
-      return {
-          route: route,
-          result: location.pathname.match(pathToRegex(route.path))
-      };
+  const potentialMatches = routes.map((route) => {
+    return {
+      route: route,
+      result: location.pathname.match(pathToRegex(route.path)),
+    };
   });
 
-  let match = potentialMatches.find(potentialMatch => potentialMatch.result !== null);
+  let match = potentialMatches.find(
+    (potentialMatch) => potentialMatch.result !== null
+  );
 
   if (!match) {
-      match = {
-          route: routes[0],
-          result: [location.pathname]
-      };
+    match = {
+      route: routes[0],
+      result: [location.pathname],
+    };
   }
 
   new match.route.view(getParams(match));
-
 };
 
 window.addEventListener("popstate", router);
 
 document.addEventListener("DOMContentLoaded", () => {
-    document.body.addEventListener("click", e => {
-        if (e.target.matches("[data-link]")) {
-            e.preventDefault();
-            navigateTo(e.target.href);
-        }
-    });
+  document.body.addEventListener("click", (e) => {
+    if (e.target.matches("[data-link]")) {
+      e.preventDefault();
+      navigateTo(e.target.href);
+    }
+  });
 
-    router();
+  router();
 });
