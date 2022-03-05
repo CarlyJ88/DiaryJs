ba96bb10881a05f11c586be752ca9bf530638adb
Carly <carly.jenkinson65@googlemail.com>
2022-03-05 15:04:09 +0000

    Add new service to select unique category by date.

--- Backend/categoryService.ts
+++ Backend/categoryService.ts
@@ -10,3 +10,13 @@ export async function listCategories(): Promise<Category[]> {
   const categories = await executeQuery('SELECT * FROM categories', []);
   return categories.rows.map((row: any) => ({ id: row.id, name: row.name, colourCode: row.colour_code }))
 }
+
+export async function listCategoriesByDate(): Promise<Category[]> {
+  const categories = await executeQuery('SELECT * FROM categories', []);
+  return categories.rows.map((row: any) => ({ id: row.id, name: row.name, colourCode: row.colour_code }))
+}
+
+export async function listCategoriesForSpeficDate(dateComingInStart: string, dateComingInEnd: string): Promise<Category[]> {
+  const entries = await executeQuery('SELECT distinct c.* FROM entries e INNER JOIN categories c ON e.category_id = c.id WHERE e.date between $1 and $2', [dateComingInStart, dateComingInEnd]);
+  return entries.rows.map((row: any) => ({ id: row.id, name: row.name, colourCode: row.colour_code }))
+}
